d21d1f19df0f63cca4a153fd39ab3e001451b232
ahmed elkhalily <a.elkhalily10@gmail.com>
2023-04-30 01:26:06 +0300

    first commit

---
+++ src/views/PolicyView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useSettingStore } from '../stores/index.js'
+import { useI18n } from 'vue-i18n';
+
+const route = useRoute()
+const settingStore = useSettingStore()
+const { locale } = useI18n({ useScope: 'global' })
+
+if (settingStore.policy.length === 0) {
+    await settingStore.getFooter()
+}
+
+const data = ref(settingStore.policy[route.params.id|| 1].page_translations.find(element => element.lang === locale.value) )
+watch(route, ()=>{
+    data.value =settingStore.policy[route.params.id|| 1].page_translations.find(element => element.lang === locale.value)
+})
+
+</script>
+
+<template>
+    <v-container >
+        <v-img v-if="data.img" :src="data.img" height="300" cover />
+        <h2 class="mt-12" v-html="data.title"></h2>
+        <v-card-text v-html="data.content"></v-card-text>
+    </v-container>
+</template>
